aeaefb5dc5fbc42f0b1d75c32715a96eee28111b
Bruna Nunes <139567240+xbrunanunes@users.noreply.github.com>
2024-01-14 13:33:49 -0300

    primeira vez.java

--- primeira vez.java
+++ primeira vez.java
@@ -1,27 +1,21 @@
-var readyToPlay = confirm("Boas vindas, pronta para jogar?");
+// Definindo os valores iniciais
+var heroName = 'Elfa da Noite';
+var hp = 86;
+var tier = 3160;
+var tminDamage = 3675;
 
-if (readyToPlay) {
-  
-    var heroName = 'Elfa da Noite';
-    var hp = 86;
-    var tier = 3160;
-    var tminDamage = 1;
+// Verificando o tier com base nas condições fornecidas
+if (tier < 1000) tier = 'Iron';
+else if (tier <= 2000) tier = 'Bronze';
+else if (tier <= 5000) tier = 'Silver';
+else if (tier <= 7000) tier = 'Gold';
+else if (tier <= 8000) tier = 'Platinum';
+else if (tier <= 9000) tier = 'Ascendant';
+else if (tier <= 10000) tier = 'Immortal';
+else tier = 'Radiant';
 
-    
-    if (tier < 1000) tier = 'Iron';
-    else if (tier <= 2000) tier = 'Bronze';
-    else if (tier <= 5000) tier = 'Silver';
-    else if (tier <= 7000) tier = 'Gold';
-    else if (tier <= 8000) tier = 'Platinum';
-    else if (tier <= 9000) tier = 'Ascendant';
-    else if (tier <= 10000) tier = 'Immortal';
-    else tier = 'Radiant';
+// Calculando o nível com base no dano do herói
+var level = Math.round(tminDamage / 100);
 
-   
-    var level = Math.round(tminDamage / 100);
-
-    
-    console.log("O herói " + heroName + " está no tier " + tier + " e está no nível " + level + ".");
-} else {
-    console.log("Até a próxima!");
-}
+// Exibindo o resultado
+console.log("O herói " + heroName + " faz parte da classe " + tier + " e está no nível " + level + ".");
