a8508a67135fd355172a587fb74c73ed83a6cbe7
Bruna Nunes <139567240+xbrunanunes@users.noreply.github.com>
2024-01-14 13:26:42 -0300

    primeira vez.java

--- primeira vez.java
+++ primeira vez.java
@@ -1,11 +1,13 @@
-
 var readyToPlay = confirm("Boas vindas, pronta para jogar?");
 
 if (readyToPlay) {
+  
     var heroName = 'Elfa da Noite';
     var hp = 86;
     var tier = 3160;
     var tminDamage = 1;
+
+    
     if (tier < 1000) tier = 'Iron';
     else if (tier <= 2000) tier = 'Bronze';
     else if (tier <= 5000) tier = 'Silver';
@@ -14,8 +16,11 @@ if (readyToPlay) {
     else if (tier <= 9000) tier = 'Ascendant';
     else if (tier <= 10000) tier = 'Immortal';
     else tier = 'Radiant';
+
+   
     var level = Math.round(tminDamage / 100);
 
+    
     console.log("O herói " + heroName + " está no tier " + tier + " e está no nível " + level + ".");
 } else {
     console.log("Até a próxima!");
